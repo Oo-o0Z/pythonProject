98207de96e15c0be47077535092dae7b59939aad
ytl142857 <2854669890@qq.com>
2021-05-27 15:57:49 +0800

    add more config

--- utils/read_config.py
+++ utils/read_config.py
@@ -1,8 +1,8 @@
 import json
 
 
-class config():
-    def __init__(self) -> None:
+class dataConfig():
+    def __init__(self):
         json_config = self.__read_config()
         self.data_path = json_config.setdefault("dataPath", "data/adult.data")
         self.qi_index = json_config.setdefault("qiIndex", [0, 1, 4, 5, 6, 8, 9, 13])
@@ -11,9 +11,21 @@ class config():
 
     def __read_config(self):
         with open("config.json") as fp:
-            return json.loads(fp.read())
+            return json.loads(fp.read())["inputData"]
+
+class modeConfig():
+    def __init__(self):
+        json_config = self.__read_config()
+        self.model = json_config.setdefault("model", "relax")
+        self.k = json_config.setdefault("k", 10)
+        self.flag = json_config.setdefault("flag", "")
+
+    def __read_config(self):
+        with open("config.json") as fp:
+            return json.loads(fp.read())["mode"]
+        
 
 if __name__ == "__main__":
-
-    conf = config()
-    print(conf.is_cat)
+    # test read config here
+    conf = modeConfig()
+    print(conf.k)
